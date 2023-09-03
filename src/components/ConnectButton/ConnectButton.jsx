037040348a18000946f7e80f8882c4d6e6b21e12
NaTaLiYa01 <nmoskalenko911@gmail.com>
2023-09-03 20:53:49 +0300

    fixes styles, fixes modal appearance

--- src/components/ConnectButton/ConnectButton.jsx
+++ src/components/ConnectButton/ConnectButton.jsx
@@ -1,12 +1,11 @@
 'use client';
 
-import PropTypes from 'prop-types';
-
 import { useState } from 'react';
+import PropTypes from 'prop-types';
 
 import data from 'data/data.json';
 import Icon from 'public/icons/connect.svg';
-import ConnectModal from '../ConnectModal/ConnectModal';
+import { ConnectModal } from '../ConnectModal';
 
 export const ConnectButton = ({ color }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -18,16 +17,21 @@ export const ConnectButton = ({ color }) => {
     <>
       <button
         type="button"
-        aria-label="Кнопка Зв'язатися"
-        className={color === "accent" ? "flex items-center gap-2 m-0 p-0 text-accent" : "flex items-center gap-2 m-0 p-0 text-black"}
+        className={`flex items-center gap-2 m-0 p-0 transition transform duration-300 ease-out hover:scale-[1.15] ${
+          color === 'accent' ? ' text-accent' : ' text-black'
+        }`}
         onClick={toggleModal}
       >
-        <Icon className="fill-current" />
-        <p className="text-font16 font-formular font-bold leading-[16px]">
+        <Icon className="fill-current w-[24px] h-[24px] xl:w-[16px] xl:h-[16px]" />
+        <p className="text-font24 xl:text-font16 font-bold leading-[16px]">
           {data.connectBtn}
         </p>
       </button>
-      {isOpen && <ConnectModal toggleModal={toggleModal} isOpen={isOpen} />}
+      {isOpen && (
+        <ConnectModal toggleModal={toggleModal} isOpen={isOpen}>
+          {/* <div>Modal Form</div> */}
+        </ConnectModal>
+      )}
     </>
   );
 };
--- src/components/ConnectModal/ConnectModal.jsx
+++ src/components/ConnectModal/ConnectModal.jsx
@@ -1,42 +1,43 @@
+import { Dialog, Transition } from '@headlessui/react';
 import PropTypes from 'prop-types';
 
-import { Dialog, Transition } from '@headlessui/react';
-import Icon from 'public/icons/close2.svg';
+import Icon from 'public/icons/closeModal.svg';
 
-export default function ConnectModal({ toggleModal, isOpen, children }) {
+export const ConnectModal = ({ toggleModal, isOpen, children }) => {
   return (
-    <Transition
-      show={isOpen}
-      enter="transition duration-300 ease-out"
-      enterFrom="transform scale-70 opacity-0"
-      enterTo="transform scale-100 opacity-100"
-      leave="transition duration-100 ease-out"
-      leaveFrom="transform scale-100 opacity-100"
-      leaveTo="transform scale-70 opacity-0"
-    >
+    <Transition appear show={isOpen}>
       <Dialog open={isOpen} onClose={toggleModal} className="relative z-50">
         <div className="fixed inset-0 bg-accent/75" aria-hidden="true">
           <div className="fixed inset-0 flex items-center justify-center">
-            <Dialog.Panel className="bg-black p-[24px] xl:p-[60px] w-[280px] h-[647px] md:w-[566px] md:h-[826px] xl:w-[724px] xl:h-[978px]">
-              <button
-                type="button"
-                aria-label="Кнопка Закрити модальне вікно"
-                className="block ml-auto w-[48px] h-[48px]"
-                onClick={toggleModal}
-              >
-                <Icon className="block mx-auto" />
-              </button>
-              {children}
-            </Dialog.Panel>
+            <Transition.Child
+              enter="transition duration-300 ease-out"
+              enterFrom="transform scale-50 opacity-0"
+              enterTo="transform scale-100 opacity-100"
+              leave="transition duration-300 ease-out"
+              leaveFrom="transform scale-100 opacity-100"
+              leaveTo="transform scale-50 opacity-0"
+            >
+              <Dialog.Panel className="bg-black p-[24px] xl:p-[60px] w-[280px] h-[647px] md:w-[566px] md:h-[826px] xl:w-[724px] xl:h-[978px]">
+                <button
+                  type="button"
+                  aria-label="Закрити"
+                  className="text-bgColor block ml-auto w-[48px] h-[48px] transition duration-300 ease-out hover:text-accent"
+                  onClick={toggleModal}
+                >
+                  <Icon className="block mx-auto w-[16px] h-[16px] md:w-[24px] md:h-[24px] fill-current" />
+                </button>
+                {children}
+              </Dialog.Panel>
+            </Transition.Child>
           </div>
         </div>
       </Dialog>
     </Transition>
   );
-}
+};
 
 ConnectModal.propTypes = {
   toggleModal: PropTypes.func.isRequired,
   isOpen: PropTypes.bool.isRequired,
-  children: PropTypes.node,
+  children: PropTypes.node.isRequired,
 };
