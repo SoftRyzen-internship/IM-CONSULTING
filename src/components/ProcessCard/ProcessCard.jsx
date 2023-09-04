558e2a0c3e6254bed5bb724365f2d23eae76d3ac
NaTaLiYa01 <nmoskalenko911@gmail.com>
2023-09-04 16:07:26 +0300

    adds process card styles

--- src/components/ProcessCard/ProcessCard.jsx
+++ src/components/ProcessCard/ProcessCard.jsx
@@ -3,22 +3,30 @@ import PropTypes from 'prop-types';
 
 export const ProcessCard = ({ number, subtitle, text, image }) => {
   return (
-    <div className="w-[280px] md:w-[696px] xl:w-[1012px] md:flex text-right even:text-left md:even:flex-row-reverse">
+    <div className="flex flex-col md:flex-row md:even:flex-row-reverse gap-[16px] md:gap-[24px] xl:gap-[32px] w-[280px] md:w-[696px] xl:w-[1012px] text-right even:text-left">
       <div>
-        <Image src={image} alt={subtitle} />
+        <Image
+          src={image}
+          alt={subtitle}
+          className="w-[280px] h-[200px] md:w-[342px] md:h-[256px] xl:w-[500px] xl:h-[376px] md:mt-[20px] "
+        />
       </div>
-      <div>
-        <div className="flex justify-between items-baseline">
-          <p className="text-orange text-[48px] font-light opacity-10">
+      <div className="w-[280px] md:w-[330px] xl:w-[480px] md:mt-[52px]">
+        <div
+          className={`flex justify-between items-baseline ${
+            number === '02' || number === '04' ? 'flex-row-reverse' : 'flex-row'
+          }`}
+        >
+          <div className="w-[59px] h-[59px] md:w-[94px] md:h-[88px] xl:w-[108px] xl:h-[100px] text-orange text-[48px] font-light tracking-[4.8px] opacity-10 md:text-[72px] md:tracking-[7.2px] xl:text-[82px] xl:tracking-[8.2px]">
             {number}
-          </p>
+          </div>
           <h3>
             <span className="text-orange">[ </span>
             <span className="mx-[12px] font-bold">{subtitle}</span>
             <span className="text-orange"> ]</span>
           </h3>
         </div>
-        <p className="font-light">{text}</p>
+        <p className="font-light md:text-font16 xl:text-font24">{text}</p>
       </div>
     </div>
   );
--- src/sections/Process/Process.jsx
+++ src/sections/Process/Process.jsx
@@ -1,14 +1,14 @@
 import { Container } from '@/components/Container';
 import { ProcessCard } from '@/components/ProcessCard';
-import data from 'data/data.json';
+import data from '@/data/data.json';
 
 export const Process = () => {
   return (
     <section>
       <Container>
-        <h2 className="invisible">Process Section</h2>
-        <p>{data.process.title}</p>
-        <ul>
+        <h2 className="sr-only">Process Section</h2>
+        <p className='mb-[48px] md:mb-[16px] xl:mb-[24px]'>{data.process.title}</p>
+        <ul className='flex flex-col gap-[36px] xl:gap-[124px]'>
           {data.process.cards.map(item => (
             <ProcessCard
               key={item.number}
