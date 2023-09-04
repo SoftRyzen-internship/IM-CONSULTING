49763cc87e85c73014e5ff5ba85988a3d1eaed73
Nadiia Khoptiuk <98387901+nadiakhoptiuk@users.noreply.github.com>
2023-09-04 11:08:57 +0300

    Revert "Feature/hero"

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,38 +4,7 @@ import { Header } from '@/layout/Header';
 import '../styles/globals.css';
 
 const formular = localFont({
-  src: [
-    {
-      path: './fonts/formular/Formular-Light.ttf',
-      weight: '300',
-      style: 'normal',
-    },
-    {
-      path: './fonts/formular/Formular.ttf',
-      weight: '400',
-      style: 'normal',
-    },
-    {
-      path: './fonts/formular/Formular-Medium.ttf',
-      weight: '500',
-      style: 'normal',
-    },
-    {
-      path: './fonts/formular/Formular-Bold.ttf',
-      weight: '700',
-      style: 'normal',
-    },
-    {
-      path: './fonts/formular/Formular-Black.ttf',
-      weight: '900',
-      style: 'normal',
-    },
-    {
-      path: './fonts/formular/Formular-Italic.ttf',
-      weight: '400',
-      style: 'italic',
-    },
-  ],
+  src: './fonts/formular/Formular.ttf',
   variable: '--font-formular',
 });
 
