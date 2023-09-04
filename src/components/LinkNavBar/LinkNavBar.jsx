5201cf1ae26b5e3aba0aba1f39e997596666e991
BogdanGlazkov <bglazkov@i.ua>
2023-09-04 23:34:06 +0300

    fix hover

--- src/components/LinkNavBar/LinkNavBar.jsx
+++ src/components/LinkNavBar/LinkNavBar.jsx
@@ -9,7 +9,7 @@ export const LinkNavBar = ({ link, label, handleMenuToggle, type }) => {
   };
 
   return (
-    <li className="flex items-center cursor-pointer duration-300">
+    <li className="flex items-center cursor-pointer">
       <Link
         key={label}
         to={link}
@@ -19,9 +19,9 @@ export const LinkNavBar = ({ link, label, handleMenuToggle, type }) => {
         onClick={handleClick}
         className={`${
           type === 'dark'
-            ? 'xl:text-gray xl:hover:text-white xl:focus:text-white duration-300'
-            : 'xl:text-black xl:opacity-25 xl:hover:opacity-100 xl:focus:opacity-100 duration-300'
-        } text-[24px] xl:text-[16px] xl:font-bold leading-4`}
+            ? 'xl:text-gray xl:hover:text-white xl:focus:text-white '
+            : 'xl:text-black xl:opacity-25 xl:hover:opacity-100 xl:focus:opacity-100 '
+        } text-[24px] xl:text-[16px] xl:font-bold leading-4 hover:text-white focus:text-white duration-300`}
         activeClass={`${
           type === 'dark' ? 'xl:text-accent' : 'xl:text-orange'
         } font-bold pointer-events-none`}
