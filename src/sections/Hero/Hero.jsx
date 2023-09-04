7fad33f910606d1ba4a0f9aeba096404ede82e7f
Sergio Nezhigay <abc1971h@gmail.com>
2023-09-04 20:26:41 +0300

    double background add

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -3,11 +3,8 @@ import { HeroContent } from '@/components/HeroContent';
 
 export const Hero = () => {
   return (
-    <section
-      id="hero"
-      className="hero relative h-auto pt-[74px] md:pt-[120px] xl:pt-[168px] overflow-hidden"
-    >
-      <Container className="relative">
+    <section id="hero" className="hero-bgcolor relative h-auto overflow-hidden">
+      <Container className="relative hero-bg pt-[74px] md:pt-[120px] xl:pt-[168px] ">
         <HeroContent />
       </Container>
     </section>
