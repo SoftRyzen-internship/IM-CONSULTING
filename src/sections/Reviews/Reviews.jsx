fcec7fee283b15ff55f2e19d798a13229cc9504b
Sergio Nezhigay <abc1971h@gmail.com>
2023-09-04 18:16:19 +0300

    reviews init

---
+++ src/sections/Reviews/Reviews.jsx
@@ -0,0 +1,11 @@
+import { Container } from '@/components/Container';
+
+export const Reviews = () => {
+  return (
+    <section id="reviews">
+      <Container>
+        <p>It is Reviews section</p>
+      </Container>
+    </section>
+  );
+};
