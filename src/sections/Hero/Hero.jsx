05d371b0f492e1b782fe040ededd6a45adc21e94
Diana Forostiana <dianaforost@gmail.com>
2023-09-04 22:06:47 +0200

    fix: fixed

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,12 +1,10 @@
 import { Container } from '@/components/Container';
-import Form from '@/components/Form/Form';
 
 export const Hero = () => {
   return (
-    <section className="bg-[#171717]">
+    <section>
       <Container>
         <p>It is hero section</p>
-        <Form />
       </Container>
     </section>
   );
