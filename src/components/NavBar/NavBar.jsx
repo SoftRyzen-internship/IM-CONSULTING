462fb7ad92b6547557676afcac44611fbbe85017
BogdanGlazkov <bglazkov@i.ua>
2023-09-04 23:20:08 +0300

    add position on desktop

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,7 +7,7 @@ export const NavBar = ({ type }) => {
   const { navBar } = data;
 
   return (
-    <div className="flex flex-col gap-[50px] xl:gap-8">
+    <div className="flex flex-col gap-[50px] xl:gap-8 mx-auto w-[168px] xl:w-[117px] xl:absolute xl:top-1/2 xl:left-0 xl:transform xl:-translate-y-1/2">
       <nav>
         <ul className="flex flex-col gap-6 xl:gap-2">
           {navBar.map(({ label, link }) => (
