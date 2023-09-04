57930f6807de7d1009e8c63cc2fe5e851ad6c22c
BogdanGlazkov <bglazkov@i.ua>
2023-09-04 20:52:41 +0300

    add navbar

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types';
+
+import { LinkNavBar } from '@/components';
+import data from '@/data/data.json';
+
+export const NavBar = ({ type }) => {
+  const { navBar } = data;
+
+  return (
+    <nav>
+      <ul className="flex flex-col">
+        {navBar.map(({ label, link }) => (
+          <LinkNavBar key={label} link={link} label={label} type={type} />
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+NavBar.propTypes = {
+  type: PropTypes.string.isRequired,
+};
