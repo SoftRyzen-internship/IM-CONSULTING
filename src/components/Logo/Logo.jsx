604e44409b10277379f419d1515e5f76956f57cf
Anastasia-front <palitsanastasia3.ap@gmail.com>
2023-09-05 13:46:02 +0300

    delete container

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -5,16 +5,15 @@ import { routes } from 'routes';
 
 export const Logo = () => {
   return (
-    <div className="w-[48px] h-[48px] xl:w-[52px] xl:h-[52px] p-[6px] xl:p-0">
-      <Link
-        href={routes.HOME}
-        aria-label="логотип"
-        className="flex items-center
-      w-[36px] h-[36px] xl:w-[52px] xl:h-[52px]
+    <Link
+      href={routes.HOME}
+      aria-label="логотип"
+      className="flex items-center
+        w-[48px] h-[48px] xl:w-[52px] xl:h-[52px] p-[6px] xl:p-0
+
       text-white hover:text-accent focus:text-accent duration-300"
-      >
-        <LogoIcon />
-      </Link>
-    </div>
+    >
+      <LogoIcon className="w-[36px] h-[36px] xl:w-[52px] xl:h-[52px]" />
+    </Link>
   );
 };
