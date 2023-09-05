062c80ae3dd9309f25db00035ac080bc139156db
Anastasia-front <palitsanastasia3.ap@gmail.com>
2023-09-05 13:40:12 +0300

    change logo.svg and add necessary styles to it
    talk with the designer and dо as we agreed with him on the sizes - on a mobile phone and a tablet, a 36x36 logo in a 48x48 container (with paddings of 6x), on a desktop - a logo and a container of the same size 52x52

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,15 +1,20 @@
 import Link from 'next/link';
+
 import LogoIcon from 'public/icons/logo.svg';
 import { routes } from 'routes';
 
 export const Logo = () => {
   return (
-    <Link
-      href={routes.HOME}
-      aria-label="логотип"
-      className="flex items-center px-[7px] py-[6px] w-[48px] h-[48px]"
-    >
-      <LogoIcon />
-    </Link>
+    <div className="w-[48px] h-[48px] xl:w-[52px] xl:h-[52px] p-[6px] xl:p-0">
+      <Link
+        href={routes.HOME}
+        aria-label="логотип"
+        className="flex items-center
+      w-[36px] h-[36px] xl:w-[52px] xl:h-[52px]
+      text-white hover:text-accent focus:text-accent duration-300"
+      >
+        <LogoIcon />
+      </Link>
+    </div>
   );
 };
