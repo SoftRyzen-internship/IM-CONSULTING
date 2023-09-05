b5bc6769d2ec27728ad81dba6bd7a28cb48153b1
NaTaLiYa01 <nmoskalenko911@gmail.com>
2023-09-05 11:35:43 +0300

    adds images to section-process

--- src/components/ProcessCard/ProcessCard.jsx
+++ src/components/ProcessCard/ProcessCard.jsx
@@ -1,13 +1,15 @@
 import Image from 'next/image';
 import PropTypes from 'prop-types';
 
-export const ProcessCard = ({ number, subtitle, text, image }) => {
+export const ProcessCard = ({ number, subtitle, text, src }) => {
   return (
     <div className="flex flex-col md:flex-row md:even:flex-row-reverse gap-[16px] md:gap-[24px] xl:gap-[32px] w-[280px] md:w-[696px] xl:w-[1012px] text-right even:text-left">
       <div>
         <Image
-          src={image}
+          src={src}
           alt={subtitle}
+          width={500}
+          height={376}
           className="w-[280px] h-[200px] md:w-[342px] md:h-[256px] xl:w-[500px] xl:h-[376px] md:mt-[20px] "
         />
       </div>
@@ -36,5 +38,5 @@ ProcessCard.propTypes = {
   number: PropTypes.string.isRequired,
   subtitle: PropTypes.string.isRequired,
   text: PropTypes.string.isRequired,
-  image: PropTypes.string.isRequired,
+  src: PropTypes.string.isRequired,
 };
--- src/sections/Process/Process.jsx
+++ src/sections/Process/Process.jsx
@@ -7,15 +7,17 @@ export const Process = () => {
     <section>
       <Container>
         <h2 className="sr-only">Process Section</h2>
-        <p className='mb-[48px] md:mb-[16px] xl:mb-[24px]'>{data.process.title}</p>
-        <ul className='flex flex-col gap-[36px] xl:gap-[124px]'>
+        <p className="mb-[48px] md:mb-[16px] xl:mb-[24px]">
+          {data.process.title}
+        </p>
+        <ul className="flex flex-col gap-[36px] xl:gap-[124px]">
           {data.process.cards.map(item => (
             <ProcessCard
               key={item.number}
               number={item.number}
               subtitle={item.subtitle}
               text={item.text}
-              image
+              src={item.src}
             />
           ))}
         </ul>
