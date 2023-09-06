a82684d47143e80ba1f64fd077e5b1a587fb39ef
Diana Forostiana <dianaforost@gmail.com>
2023-09-06 13:58:33 +0200

    fix: images & data

--- src/components/ServicesCard/ServicesCard.jsx
+++ src/components/ServicesCard/ServicesCard.jsx
@@ -9,7 +9,6 @@ export const ServicesCard = ({ title, image, text, btnText }) => {
     <li className="group relative w-[280px] h-[529px] md:w-[224px] md:h-[482px] xl:w-[324px] xl:h-[435px] xl:overflow-hidden">
       <div className="transform translate-y-0 transition-transform duration-500 ease-in-out relative hidden xl:block xl:w-[324px] z-10 group-hover:translate-y-[-100%] group-focus-within:translate-y-[-100%]">
         <Image src={image} alt={title} width={324} height={435} />
-        <div className="bg-accent absolute top-0 w-full h-full opacity-70"></div>
         <p className="absolute bottom-[48px] left-[24px] tracking-[0.04em] text-[42px] leading-[51px]">
           {title}
         </p>
--- src/components/ServicesCardList/ServicesCardList.jsx
+++ src/components/ServicesCardList/ServicesCardList.jsx
@@ -1,4 +1,4 @@
-import data from '@/data/data.json';
+import servicesData from '@/data/home/services.json';
 import { ServicesCard } from '../ServicesCard';
 
 import services1 from '/public/images/services/services1.jpg';
@@ -6,7 +6,7 @@ import services2 from '/public/images/services/services2.jpg';
 import services3 from '/public/images/services/services3.jpg';
 
 export const ServicesCardList = () => {
-  const { services } = data;
+  const { services } = servicesData;
   const { title, cards, text, btnText } = services;
   const images = [services1, services2, services3];
 
