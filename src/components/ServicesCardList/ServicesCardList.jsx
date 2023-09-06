bcbdea313479b6fbfad4186b0e7e9c994c678c70
Nadiia <holineinadia@gmail.com>
2023-09-06 17:52:01 +0300

    fix visually-hidden class, import, add sharp for prod

--- src/components/ServicesCardList/ServicesCardList.jsx
+++ src/components/ServicesCardList/ServicesCardList.jsx
@@ -1,4 +1,4 @@
-import servicesData from '@/data/home/services.json';
+import services from '@/data/home/services.json';
 import { ServicesCard } from '../ServicesCard';
 
 import services1 from '/public/images/services/services1.jpg';
@@ -6,13 +6,11 @@ import services2 from '/public/images/services/services2.jpg';
 import services3 from '/public/images/services/services3.jpg';
 
 export const ServicesCardList = () => {
-  const { services } = servicesData;
-  const { title, cards, text, btnText } = services;
+  const { cards, text, btnText } = services;
   const images = [services1, services2, services3];
 
   return (
     <>
-      <h2 className="hidden">{title}</h2>
       <ul className="flex flex-col gap-[16px] justify-center items-center md:flex-row md:gap-[12px] xl:gap-[20px]">
         {cards.map((cardTitle, index) => (
           <ServicesCard
