6890fb10f291340105a25bce5c9c67acccebc210
Diana Forostiana <dianaforost@gmail.com>
2023-09-06 13:08:16 +0200

    fix: deleted form from section

--- src/sections/Reviews/Reviews.jsx
+++ src/sections/Reviews/Reviews.jsx
@@ -1,12 +1,10 @@
 import { Container } from '@/components/Container';
-import Form from '@/components/Form/Form';
 
 export const Reviews = () => {
   return (
     <section id="reviews">
       <Container>
         <p>It is Reviews section</p>
-        <Form />
       </Container>
     </section>
   );
