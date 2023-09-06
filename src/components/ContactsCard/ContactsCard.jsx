e34a874d3950bec56f3ae955d425421ce8878c2e
NaTaLiYa01 <nmoskalenko911@gmail.com>
2023-09-06 11:50:35 +0300

    fixes contacts section styles

--- src/components/ContactsCard/ContactsCard.jsx
+++ src/components/ContactsCard/ContactsCard.jsx
@@ -2,25 +2,36 @@ import data from '@/data/data.json';
 
 export const ContactsCard = () => {
   return (
-    <div>
-      <h2>{data.contacts.title}</h2>
-      <ul>
-        <li>
-          <h3>{data.contacts.phoneText}</h3>
-          <ul>
-            {data.contacts.phone.map(item => (
-              <li key={item}>{item}</li>
-            ))}
-          </ul>
-        </li>
-        <li>
-          <h3>{data.contacts.emailText}</h3>
-          <p>{data.contacts.email}</p>
-        </li>
-        <li>
-          <h3>{data.contacts.socials}</h3>
-        </li>
-      </ul>
+    <div className="w-[280px] md:w-[320px] xl:w-[480px]">
+      <div className='mb-[64px] md:mb-[56px]'>
+        <h2 className="mx-auto text-font24 font-light leading-[32px] mb-[32px] md:mb[36px] xl:mb-[48px]">
+          <span className="text-accent">[ </span>
+          <span className="mx-[12px] font-bold">{data.contacts.title}</span>
+          <span className="text-accent"> ]</span>
+        </h2>
+        <ul>
+          <li className="flex gap-[12px] mb-[32px] md:mb[36px] xl:mb-[48px]">
+            <h3 className='text-accent font-light'>{data.contacts.phoneText}</h3>
+            <ul>
+              {data.contacts.phone.map(item => (
+                <li key={item}><p className='font-light first:mb-[4px] md:first:mb-[8px}'>{item}</p></li>
+              ))}
+            </ul>
+          </li>
+          <li className="flex gap-[12px] mb-[32px] md:mb[36px] xl:mb-[48px]">
+            <h3 className='text-accent'>{data.contacts.emailText}</h3>
+            <p>{data.contacts.email}</p>
+          </li>
+        </ul>
+      </div>
+
+      <div>
+      <h2 className="mx-auto text-font24 font-light leading-[32px] mb-[32px] md:mb[36px] xl:mb-[48px]">
+          <span className="text-accent">[ </span>
+          <span className="mx-[12px] font-bold">{data.contacts.socials}</span>
+          <span className="text-accent"> ]</span>
+        </h2>
+      </div>
     </div>
   );
 };
--- src/components/ContactsContent/ContactsContent.jsx
+++ src/components/ContactsContent/ContactsContent.jsx
@@ -2,7 +2,7 @@ import { ContactsCard } from '../ContactsCard';
 
 export const ContactsContent = () => {
   return (
-    <div className='flex flex-col md:flex-row gap-[64px] md:gap-[43px] xl:gap-[32px]'>
+    <div className='flex flex-col md:flex-row gap-[64px] md:gap-[32px]'>
       <ContactsCard />
       <div>Форма</div>
     </div>
