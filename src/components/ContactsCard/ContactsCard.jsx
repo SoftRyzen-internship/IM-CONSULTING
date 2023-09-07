c4f1666d248f34d35d532db0c9a57b736fcb631a
NaTaLiYa01 <nmoskalenko911@gmail.com>
2023-09-07 10:48:16 +0300

    adds title to contacts section

--- src/components/ContactsCard/ContactsCard.jsx
+++ src/components/ContactsCard/ContactsCard.jsx
@@ -1,15 +1,17 @@
 import { Socials } from '../Socials';
+import { Title } from '../Title';
 import data from '@/data/data.json';
 
 export const ContactsCard = () => {
   return (
     <div className="mx-auto w-[280px] md:w-[320px] xl:w-[480px]">
       <div className="mb-[64px] md:mb-[56px]">
-        <h2 className="mx-auto text-font24 font-light mb-[32px] md:mb[36px] xl:mb-[48px]">
-          <span className="text-accent">[</span>
-          <span className="mx-[12px] font-bold">{data.contacts.title}</span>
-          <span className="text-accent">]</span>
-        </h2>
+        <Title
+          tag="h2"
+          className="contacts-title mx-auto mb-[32px] md:mb[36px] xl:mb-[48px]"
+        >
+          {data.contacts.title}
+        </Title>
         <ul>
           <li className="flex gap-[12px] mb-[32px] md:mb[36px] xl:mb-[48px]">
             <h3 className="text-accent font-light">
@@ -45,11 +47,12 @@ export const ContactsCard = () => {
       </div>
 
       <div>
-        <h2 className="mx-auto text-font24 font-light mb-[32px] md:mb[36px] xl:mb-[48px]">
-          <span className="text-accent">[</span>
-          <span className="mx-[12px] font-bold">{data.contacts.socials}</span>
-          <span className="text-accent">]</span>
-        </h2>
+        <Title
+          tag="h2"
+          className="contacts-title mx-auto mb-[32px] md:mb[36px] xl:mb-[48px]"
+        >
+          {data.contacts.socials}
+        </Title>
         <Socials component="footer" />
       </div>
     </div>
--- src/components/ContactsContent/ContactsContent.jsx
+++ src/components/ContactsContent/ContactsContent.jsx
@@ -1,5 +1,6 @@
 import { ContactsCard } from '../ContactsCard';
 import { Form } from '../Form';
+import { Title } from '../Title';
 import data from '@/data/data.json';
 
 export const ContactsContent = () => {
@@ -7,11 +8,12 @@ export const ContactsContent = () => {
     <div className="flex flex-col md:flex-row gap-[64px] md:gap-[32px]">
       <ContactsCard />
       <div>
-        <h2 className="mx-auto text-font24 font-light mb-[32px] md:mb[36px] xl:mb-[48px] w-[280px] md:w-[342px] xl:w-[500px]">
-          <span className="text-accent">[</span>
-          <span className="mx-[12px] font-bold">{data.form.title}</span>
-          <span className="text-accent">]</span>
-        </h2>
+        <Title
+          tag="h2"
+          className="contacts-title mx-auto mb-[32px] md:mb[36px] xl:mb-[48px] w-[280px] md:w-[342px] xl:w-[500px]"
+        >
+          {data.form.title}
+        </Title>
         <Form />
       </div>
     </div>
