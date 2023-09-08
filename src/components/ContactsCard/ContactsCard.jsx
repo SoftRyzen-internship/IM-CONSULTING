d45e72e04576d573c433b773c5ccb01960aae0af
NaTaLiYa01 <nmoskalenko911@gmail.com>
2023-09-08 13:16:17 +0300

    fixes contacts section

--- src/components/ContactsCard/ContactsCard.jsx
+++ src/components/ContactsCard/ContactsCard.jsx
@@ -5,54 +5,50 @@ import contacts from '@/data/home/contacts.json';
 export const ContactsCard = () => {
   return (
     <div className="w-[280px] md:w-[320px] xl:w-[480px]">
-      <div className="mb-[64px] md:mb-[56px]">
-        <Title
-          tag="h3"
-          className="contacts-title mx-auto mb-[32px] md:mb-[36px] xl:mb-[48px]"
-        >
-          {contacts.title}
-        </Title>
+      <Title
+        tag="h3"
+        className="contacts-title mx-auto mb-[32px] md:mb-[36px] xl:mb-[48px]"
+      >
+        {contacts.title}
+      </Title>
 
-        <ul>
-          <li className="flex gap-[12px] mb-[32px] md:mb-[36px] xl:mb-[48px]">
-            <h3 className="text-accent font-light">{contacts.phoneText}</h3>
-            <ul>
-              {contacts.phone.map(item => (
-                <li key={item}>
-                  <a
-                    href={`tel:${item}`}
-                    className="font-light first:mb-[4px] md:first:mb-[8px] hover:text-accent focus:text-accent transition duration-300"
-                    target="_blank"
-                    rel="noreferrer noopener nofollow"
-                  >
-                    {item}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </li>
-          <li className="flex gap-[12px] mb-[32px] md:mb-[36px] xl:mb-[48px]">
-            <h3 className="text-accent">{contacts.emailText}</h3>
-            <a
-              href={`mailto:${contacts.email}`}
-              className="hover:text-accent focus:text-accent transition duration-300"
-              rel="noreferrer noopener nofollow"
-            >
-              {contacts.email}
-            </a>
-          </li>
-        </ul>
-      </div>
+      <ul className="mb-[64px] md:mb-[56px]">
+        <li className="flex gap-[12px] mb-[32px] md:mb-[36px] xl:mb-[48px]">
+          <h3 className="text-accent font-light">{contacts.phoneText}</h3>
+          <ul>
+            {contacts.phone.map(item => (
+              <li key={item}>
+                <a
+                  href={`tel:${item}`}
+                  className="font-light first:mb-[4px] md:first:mb-[8px] hover:text-accent focus:text-accent transition duration-300"
+                  target="_blank"
+                  rel="noreferrer noopener nofollow"
+                >
+                  {item}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </li>
+        <li className="flex gap-[12px]">
+          <h3 className="text-accent">{contacts.emailText}</h3>
+          <a
+            href={`mailto:${contacts.email}`}
+            className="hover:text-accent focus:text-accent transition duration-300"
+          >
+            {contacts.email}
+          </a>
+        </li>
+      </ul>
 
-      <div>
-        <Title
-          tag="h3"
-          className="contacts-title mx-auto mb-[32px] md:mb-[36px] xl:mb-[48px]"
-        >
-          {contacts.socials}
-        </Title>
-        <Socials component="footer" />
-      </div>
+      <Title
+        tag="h3"
+        className="contacts-title mx-auto mb-[32px] md:mb-[36px] xl:mb-[48px]"
+      >
+        {contacts.socials.title}
+      </Title>
+
+      <Socials component="footer" />
     </div>
   );
 };
--- src/sections/Contacts/Contacts.jsx
+++ src/sections/Contacts/Contacts.jsx
@@ -1,6 +1,7 @@
 import { Container } from '@/components/Container';
 import { ContactsContent } from '@/components/ContactsContent';
 import { labelToLink } from '@/utils/labelToLink';
+import contacts from '@/data/home/contacts.json';
 
 export const Contacts = () => {
   const id = labelToLink('Контакти');
@@ -11,7 +12,7 @@ export const Contacts = () => {
       className="bg-black text-bgColor py-[60px] md:py-[80px] xl:py-[120px]"
     >
       <Container>
-        <h2 className="visually-hidden">Contacts Section</h2>
+        <h2 className="visually-hidden">{contacts.sectionTitle}</h2>
         <ContactsContent />
       </Container>
     </section>
