829e5e41f886052d9c0cf711678ede904eb48ea8
BogdanGlazkov <bglazkov@i.ua>
2023-09-08 17:58:28 +0300

    fix width and add animation

--- src/components/MobileMenu/MobileMenu.jsx
+++ src/components/MobileMenu/MobileMenu.jsx
@@ -5,18 +5,22 @@ import { Socials } from '../Socials';
 import { Container } from '../Container';
 
 export const MobileMenu = ({ handleMenuToggle, isMenuOpen }) => {
-  return isMenuOpen ? (
-    <div className="menu-gradient absolute justify-center xl:hidden top-0 left-0 right-0 w-full h-[100vh] z-30 ">
+  return (
+    <div
+      className={`${
+        isMenuOpen ? 'translate-x-0 opacity-100' : 'opacity-0 -translate-x-full'
+      } menu-gradient absolute justify-center xl:hidden top-0 left-0 right-0 w-full h-[100vh] z-30 transition duration-300`}
+    >
       <Container className="flex">
-        <div className="flex items-center w-[34%]  md:w-[148px] h-[100vh]">
+        <div className="flex flex-shrink-0 items-center w-[68px] md:w-[88px] h-[100vh]">
           <Socials component="burgerMenu" />
         </div>
-        <div className="flex items-center md:justify-center w-full md:w-[620px] pl-6">
+        <div className="flex items-center w-full pl-6 md:pl-[240px]">
           <NavBar handleMenuToggle={handleMenuToggle} menu={true} />
         </div>
       </Container>
     </div>
-  ) : null;
+  );
 };
 
 MobileMenu.propTypes = {
