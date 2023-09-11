d5d9d75acc0927030d818b1992f8cc9e064901eb
Sergio Nezhigay <abc1971h@gmail.com>
2023-09-11 09:33:40 +0300

    parallax draft

--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -4,8 +4,8 @@ import { Socials } from '@/components/Socials';
 
 export const Header = () => {
   return (
-    <header className="header">
-      <Container>
+    <header className="header xl:absolute xl:z-10 xl:w-full xl:justify-between">
+      <Container className="xl:flex xl:justify-between xl:items-center">
         <Logo />
         <Socials component="header" />
       </Container>
