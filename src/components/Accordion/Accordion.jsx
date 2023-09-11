bf67232eba7a3cd85dc9f4b119f1d1d555c86de2
RomanPyryn <92987401+RomanPyryn@users.noreply.github.com>
2023-09-11 17:04:05 +0300

    add smooth scroll for accordion

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -1,5 +1,5 @@
 'use client';
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 // import { ButtonConsultation } from '../ButtonConsultation';
 
 export const Accordion = ({ items }) => {
@@ -7,30 +7,40 @@ export const Accordion = ({ items }) => {
   const itemRef = useRef(null);
 
   const onTitleClick = index => {
-    if (index === activeIndex) {
-      return;
-    } else {
-      setActiveIndex(index);
-      const el = itemRef.current;
-      if (el) {
-        el.scrollIntoView({ behavior: 'smooth', block: 'start' });
-      }
+    setActiveIndex(index === activeIndex ? null : index);
+    const el = itemRef.current;
+    if (el) {
+      el.scrollIntoView({ behavior: 'smooth', block: 'start' });
     }
   };
+
+  useEffect(() => {
+    const subItemRefs = itemRef.current.querySelectorAll('.subItem');
+
+    subItemRefs.forEach((subItemRef, index) => {
+      if (activeIndex === index) {
+        subItemRef.style.maxHeight = subItemRef.scrollHeight + 'px';
+      } else {
+        subItemRef.style.maxHeight = '0';
+      }
+    });
+  }, [activeIndex]);
+
   return (
     <ol className="text-bgColor leading-[1.33]" ref={itemRef}>
       {items.map((item, index) => {
         const isActive = index === activeIndex;
+
         return (
           <li
             key={item.id}
             className="relative z-[1] bg-black pt-2 -mt-2 md:pt-3 md:-mt-[14px]"
           >
             <button
-              className={`cursor-pointer w-full pt-2 md:pt-3 border-t flex justify-between gap-4 xl:gap-[482px] transition duration-300 ease-in ${
+              className={`cursor-pointer w-full pt-2 md:pt-3 border-t flex justify-between gap-4 xl:justify-start xl:gap-[482px] transition duration-300 ease-in ${
                 isActive
-                  ? 'text-accent border-accent md:justify-start md:gap-[309px]'
-                  : 'text-[#4D4D4D] border-[#4D4D4D] xl:justify-start '
+                  ? 'text-accent border-accent'
+                  : 'text-[#4D4D4D] border-[#4D4D4D] '
               }`}
               onClick={() => onTitleClick(index)}
               type="button"
@@ -42,26 +52,29 @@ export const Accordion = ({ items }) => {
                 {item.title}
               </h3>
             </button>
-            {isActive ? (
-              <div className="pt-6 pb-8 md:pt-9 md:pb-12 xl:pt-12 xl:pb-[60px] h-auto opacity-100 transition-all duration-300">
-                <ul className="space-y-2 md:space-y-4 xl:space-y-2">
-                  {item.content.map(subItem => (
-                    <li
-                      key={subItem.subtitle}
-                      className="space-y-2 md:space-y-4 pb-3 tracking-[-0.64px] md:text-[24px] md:tracking-[-0.96px] border-b border-[#4D4D4D] last:border-none"
-                    >
-                      <h4 className="text-accent border-accent">
-                        {subItem.subtitle}
-                      </h4>
-                      <p className="font-light">{subItem.text}</p>
-                    </li>
-                  ))}
-                </ul>
-                {/* <ButtonConsultation /> */}
-              </div>
-            ) : (
-              <div className="p-0 h-0 opacity-0 transition-all duration-300"></div>
-            )}
+
+            <div
+              className={`subItem box-content overflow-hidden transition-all duration-1000 ${
+                isActive
+                  ? 'pt-6 pb-8 md:pt-9 md:pb-12 xl:pt-12 xl:pb-[60px]'
+                  : 'p-0'
+              }`}
+            >
+              <ul className="space-y-2 md:space-y-4 xl:space-y-2">
+                {item.content.map(subItem => (
+                  <li
+                    key={subItem.subtitle}
+                    className="space-y-2 md:space-y-4 pb-3 tracking-[-0.64px] md:text-[24px] md:tracking-[-0.96px] border-b border-[#4D4D4D] last:border-none"
+                  >
+                    <h4 className="text-accent border-accent">
+                      {subItem.subtitle}
+                    </h4>
+                    <p className="font-light">{subItem.text}</p>
+                    {/* < ButtonConsultation /> */}
+                  </li>
+                ))}
+              </ul>
+            </div>
           </li>
         );
       })}
