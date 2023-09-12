f5f7522e5b813d736b59099d8d01a65c942e7061
Diana Forostiana <dianaforost@gmail.com>
2023-09-12 10:16:22 +0200

    fix

--- src/utils/getDictionary.js
+++ src/utils/getDictionary.js
@@ -1,7 +1,7 @@
 const dictionaries = {
   trainings: () => import('@/data/form.json').then(r => r.default),
   coaching: () => import('@/data/footer.json').then(r => r.default),
-  consulting: () => import('@/data/socials.json').then(r => r.default),
+  consulting: () => import('@/data/buttons.json').then(r => r.default),
 };
 
 export const getDictionary = async locale => {
