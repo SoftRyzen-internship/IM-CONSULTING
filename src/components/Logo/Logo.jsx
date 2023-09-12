9c8fd64258c306d0842ec0188db6616dceb98bd1
NaTaLiYa01 <nmoskalenko911@gmail.com>
2023-09-12 14:48:20 +0300

    adds refresh page by logo click

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -4,11 +4,15 @@ import LogoIcon from 'public/icons/logo.svg';
 import { routes } from 'routes';
 
 export const Logo = () => {
+  const refreshPage = () => {
+    window.location.reload();
+  };
   return (
     <Link
       href={routes.HOME}
+      onClick={refreshPage}
       aria-label="логотип"
-      className="flex items-center w-[48px] h-[48px] xl:w-[52px] xl:h-[52px] p-[6px] xl:p-0 text-white hover:text-accent focus-visible:text-accent transition duration-300 relative z-40"
+      className="flex items-center w-[48px] h-[48px] xl:w-[52px] xl:h-[52px] p-[6px] xl:p-0 text-white hover:text-accent focus:text-accent transition duration-300 relative z-40"
     >
       <LogoIcon className="w-[36px] h-[36px] xl:w-[52px] xl:h-[52px]" />
     </Link>
