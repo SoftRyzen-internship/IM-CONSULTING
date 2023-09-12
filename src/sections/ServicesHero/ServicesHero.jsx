dc9fe27583183cad8c70da438c293f22c1d3bf91
Diana Forostiana <dianaforost@gmail.com>
2023-09-12 11:25:04 +0200

    fix: styles

--- src/sections/ServicesHero/ServicesHero.jsx
+++ src/sections/ServicesHero/ServicesHero.jsx
@@ -20,7 +20,7 @@ export const ServicesHero = ({ heroContent }) => {
           : 'trainings-hero-bg'
       } h-[344px] md:h-[570px] xl:h-[743px] w-[100%] max-w-[100%] pt-[104px] md:pt-[120px] xl:pt-[141px] pb-[56px] md:pb-[126px] xl:pb-[200px]`}
     >
-      <Container className="relative overflow-hidden">
+      <Container className="flex flex-row justify-center gap-[12px] md:gap-[31px] md:justify-between relative overflow-hidden h-full">
         <ServicesHeroContent heroContent={heroContent['hero']} />
       </Container>
     </section>
