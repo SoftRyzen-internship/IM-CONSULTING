adac5a9acbb8eecb97bdfe26ebc375362d9a9726
BogdanGlazkov <bglazkov@i.ua>
2023-09-12 10:27:34 +0300

    fix color on hover

--- src/components/LinkNavBar/LinkNavBar.jsx
+++ src/components/LinkNavBar/LinkNavBar.jsx
@@ -13,14 +13,14 @@ export const LinkNavBar = ({ link, label, handleMenuToggle, isLight }) => {
         to={link}
         smooth="easeInOutQuart"
         spy={true}
-        offset={0}
+        offset={-50}
         href="/"
         onClick={handleClick}
         className={`${
           isLight
             ? 'xl:text-black'
-            : 'xl:text-gray xl:opacity-25 xl:hover:text-bgColor xl:focus:text-bgColor xl:hover:opacity-100 xl:focus:opacity-100'
-        } text-[24px] xl:text-[16px] xl:font-bold leading-4 hover:text-white focus:text-white transition duration-300`}
+            : 'xl:text-gray xl:hover:!text-bgColor xl:focus:!text-bgColor'
+        } text-[24px] xl:text-[16px] xl:font-bold xl:opacity-25 leading-4 hover:text-white focus:text-white xl:hover:text-black xl:focus:text-black xl:hover:opacity-100 xl:focus:opacity-100 transition duration-300`}
         activeClass={`${
           isLight ? 'xl:!text-orange' : 'xl:!text-accent'
         } activeLink font-bold !opacity-100 pointer-events-none`}
