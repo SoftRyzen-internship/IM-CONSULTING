1a62271a5a68ccb4c3ad9d84a238eff38093363d
Sergio Nezhigay <abc1971h@gmail.com>
2023-09-15 15:02:22 +0300

    tag canonical add

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -7,14 +7,12 @@ export const Hero = () => {
   const id = labelToLink('Головна');
 
   return (
-    <>
-      <section id={id} className=" xl:h-[707px] xl:w-full">
-        <div className="xl:h-[707px] xl:w-full xl:fixed w-full -z-10 hero-bgcolor h-[344px] md:h-[570px]">
-          <Container className="hero-bg pt-[74px] md:pt-[120px] xl:pt-[168px] h-full w-full">
-            <HeroContent />
-          </Container>
-        </div>
-      </section>
-    </>
+    <section id={id} className=" xl:h-[707px] xl:w-full">
+      <div className="xl:h-[707px] xl:w-full xl:fixed w-full -z-10 hero-bgcolor h-[344px] md:h-[570px]">
+        <Container className="hero-bg pt-[74px] md:pt-[120px] xl:pt-[168px] h-full w-full">
+          <HeroContent />
+        </Container>
+      </div>
+    </section>
   );
 };
