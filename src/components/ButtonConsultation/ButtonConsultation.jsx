9d3eb57f754ca8bc3e54b045eaea960fd87d4624
RomanPyryn <92987401+RomanPyryn@users.noreply.github.com>
2023-09-15 13:59:29 +0300

    add tabIndex for ButtonConsultation and fix text for ProgramInfo

--- src/components/ButtonConsultation/ButtonConsultation.jsx
+++ src/components/ButtonConsultation/ButtonConsultation.jsx
@@ -7,7 +7,7 @@ import { Form } from '../Form';
 import Icon from 'public/icons/consultation.svg';
 import data from '@/data/buttons.json';
 
-export const ButtonConsultation = () => {
+export const ButtonConsultation = ({ tabIdx }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleModal = () => {
@@ -20,6 +20,7 @@ export const ButtonConsultation = () => {
         type="button"
         className="flex gap-[8px] md:gap-[10px] py-[16px] w-full md:w-[500px] justify-center items-center md:ml-auto bg-accent hover:bg-darkOrange focus:bg-darkOrange transition duration-300"
         onClick={toggleModal}
+        tabIndex={tabIdx}
       >
         <span className="md:text-font24 font-medium md:tracking-[2.4px] text-black">
           {data.consultationBtn}
