747f6696490a09a582516e9201dc0f01e792a211
Diana Forostiana <dianaforost@gmail.com>
2023-09-18 17:52:53 +0200

    bugfix: deleted xxl:justify-center

--- src/sections/ServicesHero/ServicesHero.jsx
+++ src/sections/ServicesHero/ServicesHero.jsx
@@ -21,7 +21,7 @@ export const ServicesHero = ({ data }) => {
     <section
       className={`${titleClass} h-[344px] md:h-[570px] xl:h-[743px] w-[100%] max-w-[100%] pt-[104px] md:pt-[120px] xl:pt-[141px] pb-[56px] md:pb-[126px] xl:pb-[200px]`}
     >
-      <Container className="flex flex-row justify-center gap-[12px] md:gap-[31px] md:justify-between relative overflow-hidden h-full xxl:justify-center">
+      <Container className="flex flex-row justify-center gap-[12px] md:gap-[31px] md:justify-between relative overflow-hidden h-full">
         <ServicesHeroContent data={data.hero} />
       </Container>
     </section>
